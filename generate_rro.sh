48db613f860b7b17d7b9aca12dc7a5491b784e20
Arian <arian.kulmer@web.de>
2025-01-19 12:39:16 +0100

    Fix shellcheck warnings in generate_rro.sh
    Change-Id: Ia04c89f900deea8ea2c177a22ed01a3459449843

--- generate_rro.sh
+++ generate_rro.sh
@@ -5,71 +5,71 @@
 #
 
 DEBUG="${DEBUG:-0}"
-if [[ ${DEBUG} != 0 ]]; then
+if [[ $DEBUG != 0 ]]; then
     log="/dev/tty"
 else
     log="/dev/null"
 fi
 
-if [[ -z ${1} ]]; then
+if [[ -z $1 ]]; then
     echo usage: generate_rro.sh /path/to/rro.apk
     exit
 fi
 
-SRC="${1}"
+SRC="$1"
 
 # Create a temporary working directory
 TMPDIR=$(mktemp -d)
 
-name=$(basename ${SRC} | sed "s/.apk//g")
+name=$(basename "$SRC" | sed "s/.apk//g")
 
-if ! apktool d "${SRC}" -o "${TMPDIR}"/out &> "${log}"; then
-    echo "Failed to dump ${name}"
+if ! apktool d "$SRC" -o "$TMPDIR"/out &> "$log"; then
+    echo "Failed to dump $name"
     # Clear the temporary working directory
-    rm -rf "${TMPDIR}"
+    rm -rf "$TMPDIR"
     exit
 fi
 
-rm -rf ./overlay/${name}
-mkdir -p ./overlay/${name}
+rm -rf ./overlay/"$name"
+mkdir -p ./overlay/"$name"
 
 # Copy resources from apktool dump
-cp -r ${TMPDIR}/out/res ./overlay/${name}/
-rm ./overlay/${name}/res/values/public.xml
+cp -r "${TMPDIR}"/out/res ./overlay/"${name}"/
+rm ./overlay/"${name}"/res/values/public.xml
 # If public.xml was the only file in res/values remove it
-if [[ -z "$(ls -A ./overlay/${name}/res/values)" ]]; then
-    rm -rf ./overlay/${name}/res/values
+if [[ -z "$(ls -A ./overlay/"${name}"/res/values)" ]]; then
+    rm -rf ./overlay/"${name}"/res/values
 fi
 
 # Begin writing Android.bp
 printf "runtime_resource_overlay {
-    name: \"${name}\"," > ./overlay/${name}/Android.bp
+    name: \"%s\"," "$name" > ./overlay/"${name}"/Android.bp
 
 # Set theme if necessary
-theme=$(echo $SRC | sed -n "s/.*overlay\/\([a-zA-Z0-9_-]\+\)\/.*\.apk/\1/gp")
-if [[ ! -z "${theme}" ]]; then
-    printf "\n    theme: \"${theme}\"," >> ./overlay/${name}/Android.bp
+theme=$(echo "$SRC" | sed -n "s/.*overlay\/\([a-zA-Z0-9_-]\+\)\/.*\.apk/\1/gp")
+if [[ -n "${theme}" ]]; then
+    printf "\n    theme: \"%s\"," "$theme" >> ./overlay/"${name}"/Android.bp
 fi
 
 # Choose the partition
-partition=$(echo $SRC | sed -n "s/.*\/\([a-z_]\+\)\/overlay.*/\1/gp")
-if echo "product system_ext" | grep -w -q ${partition}; then
-    printf "\n    ${partition}_specific: true," >> ./overlay/${name}/Android.bp
-elif echo "odm" | grep -w -q ${partition}; then
-    printf "\n    device_specific: true," >> ./overlay/${name}/Android.bp
-elif echo "vendor" | grep -w -q ${partition}; then
-    printf "\n    vendor: true," >> ./overlay/${name}/Android.bp
+partition=$(echo "$SRC" | sed -n "s/.*\/\([a-z_]\+\)\/overlay.*/\1/gp")
+if echo "product system_ext" | grep -w -q "$partition"; then
+    printf "\n    %s_specific: true," "$partition" >> ./overlay/"${name}"/Android.bp
+elif echo "odm" | grep -w -q "$partition"; then
+    printf "\n    device_specific: true," >> ./overlay/"${name}"/Android.bp
+elif echo "vendor" | grep -w -q "$partition"; then
+    printf "\n    vendor: true," >> ./overlay/"${name}"/Android.bp
 fi
 
 # Keep raw values if necessary
 # Experimental logic: Check if there are values starting with 0 and assume that the leading 0s
 # are critical and should be kept
-if [[ ! -z $(find ./overlay/${name}/res -type f | xargs -I 'file' sed -n "/\(=\"0[0-9]\+\)/p" file) ]]; then
-    printf "\n    aaptflags: [\"--keep-raw-values\"]," >> ./overlay/${name}/Android.bp
+if [[ -n $(find ./overlay/"${name}"/res -type f -print0 | xargs -I 'file' --null sed -n "/\(=\"0[0-9]\+\)/p" file) ]]; then
+    printf "\n    aaptflags: [\"--keep-raw-values\"]," >> ./overlay/"${name}"/Android.bp
 fi
 
 # Finish the Android.bp
-printf "\n}\n" >> ./overlay/${name}/Android.bp
+printf "\n}\n" >> ./overlay/"${name}"/Android.bp
 
 # Extract attributes from AndroidManifest.xml
 manifest="${TMPDIR}/out/AndroidManifest.xml"
@@ -103,7 +103,7 @@ cat <<EOF > "$output"
     package="${package}">
     <overlay android:targetPackage="${targetPackage}"$(
 for property in "${optional_properties[@]}"; do
-    printf '\n                   %s' "$property"
+    printf "\n                   %s" "$property"
 done
 )/>
 </manifest>
