7fae2cf0d4ca2d4c1609573433db4cf14de91714
Arian <arian.kulmer@web.de>
2025-01-19 14:26:45 +0100

    Align empty line addition in Android.bp generation with the rest
    Change-Id: Ie91a992c1c82d87ea5b8718b9147001d3638e994

--- beautify_rro.sh
+++ beautify_rro.sh
@@ -269,7 +269,7 @@ if [[ -n $(head -n 1 "${RRO_DIR}/AndroidManifest.xml" | sed -n "/<manifest/p") ]
     sed -i "1 i\<!--" "${RRO_DIR}/AndroidManifest.xml"
 fi
 if [[ -n $(head -n 1 "${RRO_DIR}/Android.bp" | sed -n "/runtime_resource_overlay/p") ]]; then
-    sed -i '1i\\' "${RRO_DIR}/Android.bp"
+    sed -i "1 i\\\\" "${RRO_DIR}/Android.bp"
     sed -i "1 i\\/\/" "${RRO_DIR}/Android.bp"
     sed -i "1 i\\/\/ SPDX-License-Identifier: Apache-2.0" "${RRO_DIR}/Android.bp"
     sed -i "1 i\\/\/ SPDX-FileCopyrightText: $(date +%Y) The LineageOS Project" "${RRO_DIR}/Android.bp"
